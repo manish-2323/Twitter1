a8fa96126f5345927b3fba412da87f986ed63807
manish-2323 <Manishgarg4884@gmail.com>
2024-09-24 16:35:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': '#1DA1F2',
+      },
+      fontSize: {
+        'icon-large': '40px',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
